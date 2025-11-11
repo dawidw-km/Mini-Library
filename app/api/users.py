202ff1da428f7ff2c25cb02e5aa6b9d112d98653
dawidw-km <dawidwieclaw@hotmail.com>
2025-11-11 10:50:42 +0100

    hashing

--- app/api/users.py
+++ app/api/users.py
@@ -1,26 +1,17 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.db.session import get_db
-from app.models.user import User
 from app.schemas.user import UserCreate, UserRead
+from app.services.user_services import create_user
+
 
 router = APIRouter(prefix="/users", tags=["users"])
 
 @router.post("/", response_model=UserRead)
-def create_user(user: UserCreate, db: Session = Depends(get_db)):
-    new_user = User(
-        login=user.login,
-        password=user.password,
-        full_name=user.full_name,
-        birth_date=user.birth_date,
-        city=user.city,
-        street=user.street,
-        postal_code=user.postal_code,
-        address_email=user.address_email
-    )
-
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
+def register_user(user: UserCreate, db: Session = Depends(get_db)):
+    try:
+        new_user = create_user(db, user)
+        return new_user
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,36 +1,6 @@
 from pydantic import BaseModel, EmailStr, field_validator
 from datetime import date, datetime
-
-def ensure_not_empty(value: str, field_name: str):
-    value = value.strip()
-    if not value:
-        raise ValueError(f"Field {field_name} cannot be empty.")
-    return value
-
-def ensure_min_length(value: str, field_name: str, *, min_length: int = None):
-    if min_length and len(value) < min_length:
-        raise ValueError(f"Field {field_name} must contain at least {min_length} characters.")
-    return value
-
-def ensure_max_length(value: str, field_name: str, *, max_length: int = None):
-    if max_length and len(value) > max_length:
-        raise ValueError(f"Field {field_name} must contain at most {max_length} characters.")
-    return value
-
-def ensure_alnum(value: str, field_name: str, *, alnum: bool = False) -> str:
-    if alnum and not value.isalnum():
-        raise ValueError(f"Field {field_name} can contain only alphanumeric characters.")
-    return value
-
-def validate_text(value: str, field_name: str, *,
-                 min_length: int = None,
-                 max_length: int = None,
-                 alnum: bool = False) -> str:
-    value = ensure_not_empty(value, field_name)
-    value = ensure_min_length(value, field_name, min_length=min_length)
-    value = ensure_max_length(value, field_name, max_length=max_length)
-    value = ensure_alnum(value, field_name, alnum=alnum)
-    return value
+from app.utils.validation_utils import validate_text
 
 class UserBase(BaseModel):
     login: str
