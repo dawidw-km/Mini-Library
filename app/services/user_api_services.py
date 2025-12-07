1653bb75237b741020b8beedb4da3be7ede736ba
dawidw-km <dawidwieclaw@hotmail.com>
2025-12-08 00:10:07 +0100

    User validation fix

---
+++ app/services/user_api_services.py
@@ -0,0 +1,32 @@
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+from app.schemas.user import UserUpdate
+from app.models.user import User
+from app.services.user_services import create_user
+
+
+def add_user(db, user):
+    try:
+        new_user = create_user(db, user)
+        return new_user
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+def partial_update_user_service(
+    db: Session,
+    user_id: int,
+    user_data: UserUpdate
+):
+    user = db.query(User).filter(User.id == user_id).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+
+    for key, value in user_data.model_dump(exclude_unset=True).items():
+        setattr(user, key, value)
+
+    db.commit()
+    db.refresh(user)
+    return user
